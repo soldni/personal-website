64ca9883296f16c96eda5af593a6341690fafde2
Luca Soldaini <luca@soldaini.net>
2026-02-06 13:20:59 -0800

    minimal updates+add

---
+++ src/personal_website/__init__.py
@@ -0,0 +1,11 @@
+import sys
+
+from hugo.cli import __call as hugo_cli_entrypoint
+
+__all__ = ["hugo_cli_entrypoint"]
+
+
+def preview():
+    preview_cli = ["server", "--disableFastRender", "-D"]
+    sys.argv = sys.argv[:1] + preview_cli + [k for k in sys.argv[1:] if k not in preview_cli]
+    hugo_cli_entrypoint()
